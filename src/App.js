e0b51827a2a527c37c1c664c3d4ae0f6f53d45f6
askmary <mariana.reinaldo2003@gmail.com>
2022-07-11 10:23:07 -0300

    desafio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+import Foto from "./img/tomate.jpg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+    
+    state = {
+        nome: "Luccas Correa",
+        idade: 20,
+        comida: "farofinha de bacon",
+        musicas: ["Bonekinha", "Guren no Yumiya", "Tive Razão"]
+    }
+    render(){
+        return(
+            <div>
+               <h1>Fala pessoal, meu nome é {this.state.nome}, seja bem-vindo ao meu canal!</h1>
+               <h2>Tenho {this.state.idade} anos, clique em se inscrever!</h2>
+               <h3>E minha comida favorita é {this.state.comida}!</h3>
+               <ol>
+                <li>{this.state.musicas[0]}</li>
+                <li className="item">{this.state.musicas[1]}</li>
+                <li>{this.state.musicas[2]}</li>
+               </ol>
+               <figure>
+                <a href="https://www.google.com/search?q=tomate&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjS-_KU8fD4AhXDqpUCHdUWCNoQ_AUoAXoECAIQAw&biw=1366&bih=649&dpr=1#imgrc=9505AX09Ee2jTM">     
+                    <img src={Foto} alt="tomate"/>
+                </a>
+               </figure>
+            </div>
+        )
+    }
 }
-
-export default App;
